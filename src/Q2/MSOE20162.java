dbf68cc237a24b948da2d002cfb1e6eeedbfedbe
chubak.b <chubak.b@stu.janesville.k12.wi.us>
2023-11-16 11:25:18 -0600

    Update 11/16

--- src/Q2/MSOE20162.java
+++ src/Q2/MSOE20162.java
@@ -5,15 +5,19 @@ import java.util.*;
 public class MSOE20162 {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
-        System.out.println("Enter side length A: ");
+        System.out.print("Enter side length A: ");
         double sideA = input.nextDouble();
-        System.out.println("Enter side length C: ");
+        System.out.print("Enter side length C: ");
         double sideC = input.nextDouble();
-        System.out.println("Enter angle measure A (in degrees): ");
+        System.out.print("Enter angle measure A (in degrees): ");
         double angleA = input.nextDouble();
-        double angleC = Math.asin(Math.sin(angleA)*sideC/sideA);
+        double angleC = Math.sin(angleA);
+        angleC = angleC / angleA;
+        angleC = angleC * sideC;
+        angleC = Math.asin(angleC);
+        angleC = Math.toDegrees(angleC);
         double angleC2 = 180 - angleC;
         System.out.print(angleC + " degrees");
-        if (angleA + angleC2 < 180) { System.out.println("and " + angleC2 + " degrees"); }
+        if (angleA + angleC2 < 180) { System.out.println(" and " + angleC2 + " degrees"); }
     }
 }
