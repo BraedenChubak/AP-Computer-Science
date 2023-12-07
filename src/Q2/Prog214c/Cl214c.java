9366348cd8d2916febdf1f1747d3dbd31bfd3041
chubak.b <chubak.b@stu.janesville.k12.wi.us>
2023-12-07 11:25:41 -0600

    Update 12/7

--- src/Q2/Prog214c/Cl214c.java
+++ src/Q2/Prog214c/Cl214c.java
@@ -1,4 +1,45 @@
 package Q2.Prog214c;
 
 public class Cl214c {
+    private String grade;
+    private double gallons;
+    private double preWashCost;
+    private boolean wash;
+    private double washCost;
+    private double totalCost;
+
+    public Cl214c(String g, double gl, String w) {
+        grade = g;
+        gallons = gl;
+        if (w.equals("Y")) { wash = true; }
+        else { wash = false; }
+        preWashCost = 0;
+        washCost = 0;
+        totalCost = 0;
+    }
+
+    public void calc() {
+        if (grade.equals("R")) {
+            preWashCost = gallons * 1.359;
+        }
+        else if (grade.equals("P")) {
+            preWashCost = gallons * 1.479;
+        }
+        else if (grade.equals("H")) {
+            preWashCost = gallons * 1.429;
+        }
+        if (wash) {
+            if (gallons > 10 && gallons < 20) {
+                washCost = 02 * ((20 - Math.floor(gallons)) / 10);
+            }
+            if (gallons < 10) {
+                washCost = 2;
+            }
+        }
+        totalCost = preWashCost + washCost;
+    }
+
+    public String toString() {
+        return String.format("%.1f %s gallons:\t$%.2f\nCar Wash? %b:\t$%.2f\nTotal:\t$%.2f\n",gallons,grade,preWashCost,wash,washCost,totalCost);
+    }
 }
--- src/Q2/Prog214c/Prog214c.java
+++ src/Q2/Prog214c/Prog214c.java
@@ -1,7 +1,48 @@
 package Q2.Prog214c;
 
+import java.io.File;
+import java.io.IOException;
+import java.util.Scanner;
+import java.util.ArrayList;
+
 public class Prog214c {
     public static void main(String[] args) {
+        try {
+            Scanner input = new Scanner(new File("Langdat/prog214c.dat"));
+            ArrayList<Cl214c> customers = new ArrayList<>();
 
+            while (input.hasNext()) {
+                String grade = input.next();
+                double gallons = input.nextDouble();
+                String wash = input.next();
+                Cl214c current = new Cl214c(grade, gallons, wash);
+                customers.add(current);
+            }
+
+            for (int lcv = 0; lcv < customers.size(); lcv++) {
+                customers.get(lcv).calc();
+                System.out.println(customers.get(lcv));
+            }
+
+        } catch (IOException e) {
+            System.out.println("Can't find data file!");
+        }
     }
 }
+/*
+15.0 P gallons:	$22.19
+Car Wash? false:	$0.00
+Total:	$22.19
+
+21.4 R gallons:	$29.08
+Car Wash? true:	$0.00
+Total:	$29.08
+
+7.5 H gallons:	$10.72
+Car Wash? true:	$2.00
+Total:	$12.72
+
+17.9 R gallons:	$24.33
+Car Wash? true:	$0.60
+Total:	$24.93
+ */
