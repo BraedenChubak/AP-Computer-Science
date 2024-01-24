41a18171c4bdce29b244e0ccf4e2be363f3aad71
chubak.b <chubak.b@stu.janesville.k12.wi.us>
2024-01-24 11:37:36 -0600

    Update 1/24

--- src/Q3/Prog701g/Prog701g.java
+++ src/Q3/Prog701g/Prog701g.java
@@ -12,10 +12,66 @@ public class Prog701g {
             Scanner input = new Scanner(new File("Langdat/prog701g.dat"));
             List<Person> list = new ArrayList<Person>();
 
+            int num = input.nextInt();
+            while (num != 99) {
+                String f = input.next();
+                String l = input.next();
+                if (num == 1) {
+                    double gpa = input.nextDouble();
+                    Person kyle = new Student(f, l, gpa);
+                    list.add(kyle);
+                }
+                else if (num == 2) {
+                    int numStu = input.nextInt();
+                    Person szelogowski = new Teacher(f, l, numStu);
+                    list.add(szelogowski);
+                }
+                else if (num == 3) {
+                    String favWord = input.next();
+                    Person bob = new Admin(f, l, favWord);
+                    list.add(bob);
+                }
+                num = input.nextInt();
+            }
 
+            double tot = 0;
+            double cnt = 0;
+            int totstus = 0;
+            String large = "";
+            String small = "LLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLL";
+
+            for (Person x : list) {
+                if (x instanceof Student) {
+                    tot += ((Student)x).getGpa();
+                    cnt++;
+                }
+                if (x instanceof Teacher) {
+                    totstus += ((Teacher)x).getStudents();
+                }
+                if (x instanceof Admin) {
+                    String word = ((Admin)x).getFavw();
+                    if (word.length() > large.length()) {
+                        large = word;
+                    }
+                    if (word.length() < small.length()) {
+                        small = word;
+                    }
+                }
+            }
+
+            System.out.printf("Average student GPA: %.2f\n", tot/cnt);
+            System.out.println("Total number of students taught by teachers: " + totstus);
+            System.out.println("Smallest favorite Admin word: " + small);
+            System.out.println("Largest favorite Admin word: " + large);
 
         } catch (IOException e) {
             System.out.println("Can't find data file!");
         }
     }
 }
+/*
+Average student GPA: 3.06
+Total number of students taught by teachers: 316
+Smallest favorite Admin word: done
+Largest favorite Admin word: coolbeans
+ */
