716072ff17d53826b3e2b04eea8b309fccc25e79
chubak.b <chubak.b@stu.janesville.k12.wi.us>
2024-01-31 11:36:00 -0600

    Update 1/31

--- src/Q3/Prog492h.java
+++ src/Q3/Prog492h.java
@@ -20,21 +20,134 @@ public class Prog492h {
             life[rowCount] = tempString;
             rowCount++;
         }
+
         int genCount = 0;
-        while (genCount < 16) {
+        while (genCount < 16) { // start of generation loop
+
             System.out.println("Generation " + genCount + ":");
-            for (int r = 0; r < 30; r++) {
+            for (int r = 0; r < 30; r++) { // prints every cell
                 for (int c = 0; c < 30; c++) {
                     System.out.print(life[r][c]);
                 }
                 System.out.println();
             }
-            for (int r = 0; r < 30; r++) {
-                for (int c = 0; c < 30; c++) {
 
+            for (int r = 0; r < 30; r++) { // loop to check for births and deaths
+                for (int c = 0; c < 30; c++) {
+                    // start of finding neighbors
+                    int neighbors = 0;
+                    if (c == 0) { // left wall
+                        if (r == 0) { // top left corner
+                            for (int r2 = r; r2 < r+2; r2++) {
+                                for (int c2 = c; c2 < c+2; c2++) {
+                                    if (life[r2][c2].equals("*") || life[r2][c2].equals("D")) {
+                                        neighbors++;
+                                    }
+                                }
+                            }
+                        }
+                        else if (r == 29) { // bottom left corner
+                            for (int r2 = r; r2 > r-2; r2--) {
+                                for (int c2 = c; c2 < c+2; c2++) {
+                                    if (life[r2][c2].equals("*") || life[r2][c2].equals("D")) {
+                                        neighbors++;
+                                    }
+                                }
+                            }
+                        } else { // non-corner left wall
+                            for (int r2 = r-1; r2 < r+2; r2++) {
+                                for (int c2 = c; c2 < c+2; c2++) {
+                                    if (life[r2][c2].equals("*") || life[r2][c2].equals("D")) {
+                                        neighbors++;
+                                    }
+                                }
+                            }
+                        }
+                    }
+                    else if (c == 29) { // right wall
+                        if (r == 0) { // top right corner
+                            for (int r2 = r; r2 < r+2; r2++) {
+                                for (int c2 = c; c2 > c - 2; c2--) {
+                                    if (life[r2][c2].equals("*") || life[r2][c2].equals("D")) {
+                                        neighbors++;
+                                    }
+                                }
+                            }
+                            
+                        }
+                        else if (r == 29) { // bottom right corner
+                            for (int r2 = r; r2 > r-2; r2--) {
+                                for (int c2 = c; c2 > c-2; c2--) {
+                                    if (life[r2][c2].equals("*") || life[r2][c2].equals("D")) {
+                                        neighbors++;
+                                    }
+                                }
+                            }
+                        } else { // non-corner right wall
+                            for (int r2 = r-1; r2 < r+2; r2++) {
+                                for (int c2 = c; c2 > c-2; c2--) {
+                                    if (life[r2][c2].equals("*") || life[r2][c2].equals("D")) {
+                                        neighbors++;
+                                    }
+                                }
+                            }
+                        }
+                    }
+                    else if (r == 0) { // non-corner top wall
+                        for (int r2 = r; r2 < r+2; r2++) {
+                            for (int c2 = c - 1; c2 < c + 2; c2++) {
+                                if (life[r2][c2].equals("*") || life[r2][c2].equals("D")) {
+                                    neighbors++;
+                                }
+                            }
+                        }
+                    }
+                    else if (r == 29) { // non-corner bottom wall
+                        for (int r2 = r; r2 > r-2; r2--) {
+                            for (int c2 = c-1; c2 < c+2; c2++) {
+                                if (life[r2][c2].equals("*") || life[r2][c2].equals("D")) {
+                                    neighbors++;
+                                }
+                            }
+                        }
+                    }
+                    else if (c!=0 && c!=29 && r!=0 && r!=29) { // middle cell
+                        for (int r2 = r-1; r2 < r+2; r2++) {
+                            for (int c2 = c-1; c2 < c+2; c2++) {
+                                if (life[r2][c2].equals("*") || life[r2][c2].equals("D")) {
+                                    neighbors++;
+                                }
+                            }
+                        }
+                    }
+                    // end of finding neighbors here
+                    if (life[r][c].equals("*")) { // if there is a cell
+                        neighbors--; // finding neighbors counted its own cell, this fixes wrong calculations
+                        if (neighbors < 2) {
+                            life[r][c] = "D"; // D for dying cells that must still be counted
+                        }
+                        if (neighbors > 3) {
+                            life[r][c] = "D";
+                        }
+                    }
+                    if (life[r][c].equals(" ")) { // if there's no cell
+                        if (neighbors == 3) {
+                            life[r][c] = "B"; // B for cells being born that cannot be counted yet
+                        }
+                    }
                 }
             }
 
+            for (int r = 0; r < 30; r++) { // loop to replace "D" and "B" with actual cells
+                for (int c = 0; c < 30; c++) {
+                    if (life[r][c].equals("D")) {
+                        life[r][c] = " ";
+                    }
+                    else if (life[r][c].equals("B")) {
+                        life[r][c] = "*";
+                    }
+                }
+            }
             genCount++;
         }
     }
